e4d72f82c6dd12be66d48df8ed9e45e2ae744e0a
Kamran <mdkamran0109@gmail.com>
2023-10-16 01:13:59 +0530

    Setting up User Schema and rendering pages for signIn and SignUp

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,3 +6,18 @@ module.exports.profile=function(req,res)
         title:"Users"
     });
 }
+
+// render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Finsta|Sign Up"
+    });
+}
+
+// render the sign in page
+module.exports.signIn=function(req,res)
+{
+    return res.render('user_sign_in',{
+        title:"Finsta|Sign In"
+    });
+}
