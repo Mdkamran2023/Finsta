3f5ebaf152cbf37b0ae6bde24437b141484eb96a
Kamran <mdkamran0109@gmail.com>
2023-10-22 16:41:01 +0530

    adding passport and passport-local for the authentication purpose

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -22,35 +22,8 @@ module.exports.signIn = function (req, res) {
   });
 };
 
+
 // get the Sign Up data
-// module.exports.create = function (req, res) {
-//   if (req.body.password != req.body.confirm_password) {
-//     return res.redirect("back");
-//   }
-
-//   User.findOne({ email: req.body.email }, function (err, user) {
-//     if (err) {
-//       console.log("error in finding user in signing up");
-//       return;
-//     }
-//     if (!user) {
-//       User.create(req.body, function (err, user) {
-//         if (err) {
-//           console.log("error in creating user  while in signing up");
-//           return;
-//         }
-//         return res.redirect('/users/sign-in');
-//       });
-//     }
-
-//     else{
-//         return res.redirect('back');
-//     }
-//   });
-// };
-
-// const User = require('../models/user'); // Import your User model
-
 module.exports.create = (req, res) => {
   if (req.body.password !== req.body.confirm_password) {
     return res.redirect("back");
