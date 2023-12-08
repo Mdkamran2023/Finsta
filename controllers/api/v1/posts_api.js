7986bd227311dda4223b0c07e066db074f45f09a
Kamran <mdkamran0109@gmail.com>
2023-12-08 23:41:55 +0530

    Added version1 in Finsta web application

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,57 @@
+const Post=require("../../../models/post");
+const Comment=require("../../../models/comment");
+
+module.exports.index= async function(req,res)
+{
+let posts=await Post.find({})
+.sort('-createdAt')
+.populate({
+    path:'user',
+    select: '-password', //Exclude the password field
+})
+.populate({
+     path:'comments',
+     populate:{
+        path:'user',
+        select:"-password",
+     }
+});
+
+    return res.json(200,{
+        message:"List of posts",
+        posts:posts
+    })
+}
+
+module.exports.destroy = async function (req, res) {
+    try {
+      const post = await Post.findById(req.params.id);
+  
+      if (!post) {
+        return res.status(404).send("Post not found");
+      }
+  
+      // Check if the current user is the owner of the post
+    //   if (post.user.toString() === req.user.id) {
+        // Remove the post and associated comments
+        await Promise.all([
+          Post.deleteOne({ _id: req.params.id }),
+          Comment.deleteMany({ post: req.params.id })
+        ]);
+  
+    
+        return res.json(200,{
+           message:"Post and associated comments deleted successfully !" 
+        });
+    //   } 
+    //   else {
+    //     req.flash('error','You cannot delete this post')
+    //     return res.status(403).send("Unauthorized");
+    //   }
+    } catch (err) {
+        console.log("******",err);
+    return res.json(500,{
+       message:"Internal Server Error" 
+    });
+    }
+  };
--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,8 @@ router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
 
+router.use('/api',require('./api'));
+
 // for any further routes,access from here
 // router.use('/routername',require(./routerfile')); 
 
